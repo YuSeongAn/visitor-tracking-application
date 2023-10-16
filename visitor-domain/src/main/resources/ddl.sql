e69e4185a771381063c1dbb153cd1830fb167533
yuseongan <dhaorktm125@naver.com>
2023-10-16 22:32:10 +0900

    Change DDL

--- visitor-domain/src/main/resources/ddl.sql
+++ visitor-domain/src/main/resources/ddl.sql
@@ -9,9 +9,9 @@ create table SiteVisitorStatistic
     id               bigint auto_increment primary key,
     siteId           bigint         not null,
     periodType       varchar(30)    not null,
-    standardDateTime datetime       not null,
+    standardDateTime datetime       null,
     visitorCount     decimal(10, 0) not null
 );
 
 CREATE INDEX idx_site_root_path ON Site(siteRootPath);
-CREATE INDEX idx_site_id_period_type ON SiteVisitorStatistic(siteId, periodType);
+CREATE INDEX standard_datetime_idx_site_id ON SiteVisitorStatistic(standardDateTime, siteId);
